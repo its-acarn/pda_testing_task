adcac6ef9ca7ca4fda6107603de5ca0de65079eb
svvishjs <andreww92@live.com>
2020-11-17 12:36:32 +0000

    added some new tests to check cards_total function

--- tests/card_game_test.py
+++ tests/card_game_test.py
@@ -9,19 +9,27 @@ class CardGameTest(unittest.TestCase):
         self.card1 = Card("Spades", 1)
         self.card2 = Card("Hearts", 6)
 
+
     def test_checkForAce__true(self):
         check_result = CardGame.checkforAce(self, self.card1)
         self.assertEqual(True, check_result)
+
 
     def test_checkForAce__false(self):
         check_result = CardGame.checkforAce(self, self.card2)
         self.assertEqual(False, check_result)
 
+    
     def test_checkHighestCard__card2_higher(self):
         check_result = CardGame.highest_card(self, self.card1, self.card2)
         self.assertEqual(self.card2, check_result)
 
+    
     def test_checkHighestCard__card3_higher(self):
         card3 = Card("Spades", 10)
         check_result = CardGame.highest_card(self, self.card3, self.card2)
         self.assertEqual(self.card3, check_result)
+
+    
+    def test_cards_total(self):
+        self.assertEqual("You have a total of 7", CardGame.cards_total())
